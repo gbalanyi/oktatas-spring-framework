74dc49c2e2725dfe862c05a467372c45a7899fa9
Balanyi Gábor <balanyi.g@gmail.com>
2021-08-02 22:26:32 +0200

    04. @Autowired alapú field dependency injection.

--- SpringFrameworkDemo/src/main/java/io/github/gbalanyi/spring/framework/service/MySecondSpringService.java
+++ SpringFrameworkDemo/src/main/java/io/github/gbalanyi/spring/framework/service/MySecondSpringService.java
@@ -1,15 +1,13 @@
 package io.github.gbalanyi.spring.framework.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MySecondSpringService {
 
-    private final MyFirstSpringService myFirstSpringService;
-
-    public MySecondSpringService(MyFirstSpringService myFirstSpringService) {
-        this.myFirstSpringService = myFirstSpringService;
-    }
+    @Autowired
+    private MyFirstSpringService myFirstSpringService;
 
     public void greeting(String name) {
         System.out.println(String.format("%s Hello %s!", myFirstSpringService.getCurrentTimestamp(), name));
