9c2cae62d7775a0250109dc3c315a2ca9d8361df
Balanyi Gábor <balanyi.g@gmail.com>
2021-08-02 10:08:18 +0200

    02. Bean létrehozás explicit módon.

--- SpringFrameworkDemo/src/main/java/io/github/gbalanyi/spring/framework/config/BaseApplicationConfig.java
+++ SpringFrameworkDemo/src/main/java/io/github/gbalanyi/spring/framework/config/BaseApplicationConfig.java
@@ -1,9 +1,16 @@
 package io.github.gbalanyi.spring.framework.config;
 
+import io.github.gbalanyi.spring.framework.service.MyFirstSpringService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "io.github.gbalanyi.spring.framework")
 public class BaseApplicationConfig {
+
+    @Bean
+    public MyFirstSpringService myFirstSpringService() {
+        return new MyFirstSpringService("Spring Service");
+    }
 }
--- SpringFrameworkDemo/src/main/java/io/github/gbalanyi/spring/framework/service/MyFirstSpringService.java
+++ SpringFrameworkDemo/src/main/java/io/github/gbalanyi/spring/framework/service/MyFirstSpringService.java
@@ -5,7 +5,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class MyFirstSpringService {
 
+    private String serviceName;
+
+    public MyFirstSpringService(String serviceName){
+        this.serviceName = serviceName;
+    }
+
     public void greeting(String name) {
-        System.out.println(String.format("Hello %s!", name));
+        System.out.println(String.format("Hello %s! I'm %s", name, serviceName));
     }
 }
