11edf1c7cf7f31dd6e1fe47aa7603d80b28077ed
Balanyi Gábor <balanyi.g@gmail.com>
2021-08-04 11:51:48 +0200

    06. Egyszerű Java bean létrehozása explicit factory metódussal.

--- SpringFrameworkDemo/src/main/java/io/github/gbalanyi/spring/framework/config/BaseApplicationConfig.java
+++ SpringFrameworkDemo/src/main/java/io/github/gbalanyi/spring/framework/config/BaseApplicationConfig.java
@@ -1,9 +1,17 @@
 package io.github.gbalanyi.spring.framework.config;
 
+import java.time.Clock;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "io.github.gbalanyi.spring.framework")
 public class BaseApplicationConfig {
+
+    @Bean
+    public Clock clock() {
+        return Clock.systemUTC();
+    }
 }
--- SpringFrameworkDemo/src/main/java/io/github/gbalanyi/spring/framework/service/MyFirstSpringService.java
+++ SpringFrameworkDemo/src/main/java/io/github/gbalanyi/spring/framework/service/MyFirstSpringService.java
@@ -1,6 +1,8 @@
 package io.github.gbalanyi.spring.framework.service;
 
+import java.time.Clock;
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
@@ -9,7 +11,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class MyFirstSpringService {
 
+    private final Clock clock;
+
+    public MyFirstSpringService(Clock clock) {
+        this.clock = clock;
+    }
+
     public String getCurrentTimestamp() {
-        return DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS));
+        return DateTimeFormatter.ISO_ZONED_DATE_TIME.format(ZonedDateTime.now(clock).truncatedTo(ChronoUnit.SECONDS));
     }
 }
